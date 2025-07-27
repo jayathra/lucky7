968a162009de20bd7667a4eeda6ceb2b707ef955
Jayathra Abeywarna <jayathra@users.noreply.github.com>
2025-07-27 12:45:47 -0400

    Added logic based validations to the inputs fields so that unattainable targets and numbers of dice cannot be selected

--- src/Inputs.jsx
+++ src/Inputs.jsx
@@ -2,16 +2,19 @@ import './Inputs.css';
 import { rollDie } from './utils';
 
 export default function Inputs({ inputs, setInputs}) {
+    
     return(
         <>
             <div>
                 <label htmlFor="target">Target</label>
-                <input id="target" className="inputs" type="number" placeholder="target" value={inputs.target} onChange={(e)=>setInputs({...inputs, target: Number(e.target.value)})} />
+                <input id="target" min={inputs.dice} max={inputs.dice * 6} className="inputs" type="number" placeholder="target" value={inputs.target} onChange={(e)=>setInputs({...inputs, target: Number(e.target.value)})} />
             </div>
             <div>
                 <label htmlFor="numDice">Number of Dice</label>
-                <input id="numDice" className="inputs" type="number" placeholder="number of dice" value={inputs.dice} onChange={(e)=> {
-                    setInputs({...inputs, dice: Number(e.target.value), roll: rollDie(Number(e.target.value))});
+                <input id="numDice" min="1" className="inputs" type="number" placeholder="number of dice" value={inputs.dice} onChange={(e)=> {
+                    setInputs({...inputs, dice: Number(e.target.value), roll: rollDie(Number(e.target.value)), target: (inputs.target < Number(e.target.value) || inputs.target > Number(e.target.value) * 6) 
+                        ? Number(e.target.value) 
+                        : inputs.target});
                 }} />
             </div> 
         </>
